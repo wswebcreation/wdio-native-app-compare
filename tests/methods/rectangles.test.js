dbd836dbc3ffa16c2630a8969adeb96dcd33d307
Wim Selles <wim@wswebcreation.nl>
2020-04-23 17:45:10 +0200

    feat: fix iOS 13 issue by defaulting back to defaults
    - update dependencies
    - fix UT's

--- tests/methods/rectangles.test.js
+++ tests/methods/rectangles.test.js
@@ -1,27 +1,46 @@
-import * as Rectangles from '../../lib/methods/rectangles'
 import * as DeviceInfo from '../../lib/methods/getDeviceInfo'
-import { IMAGE_STRING } from '../mocks/mocks'
+import {IMAGE_STRING} from '../mocks/mocks'
 
 describe('rectangles', () => {
-    let getDeviceInfoSpy, ignoreRectanglesOptions
+    let getDeviceInfoSpy, ignoreRectanglesOptions, Rectangles
 
     beforeEach(() => {
+        jest.isolateModules(() => {
+            Rectangles = require('../../lib/methods/rectangles')
+        })
         delete global.browser
         delete global.$$
 
-        global.$$ = () => [ { 'element-selector-1': {} } ]
+        global.$$ = () => [{'element-selector-1': {}}]
         global.browser = {
-            getElementRect: jest.fn().mockResolvedValue({ x: 1, y: 2, width: 10, height: 20 }),
-            getSettings: jest.fn().mockResolvedValue({ allowInvisibleElements: false }),
+            getElementRect: jest.fn().mockResolvedValue({x: 1, y: 2, width: 10, height: 20}),
+            getSettings: jest.fn().mockResolvedValue({allowInvisibleElements: false}),
             updateSettings: jest.fn().mockResolvedValue({}),
             isIOS: false,
             isAndroid: false,
         }
         getDeviceInfoSpy = jest.spyOn(DeviceInfo, 'getDeviceInfo').mockResolvedValue({
             dpr: 2,
-            screenshotWidth: 812,
             isIphoneXSeries: true,
             isLargeIphoneXSeries: false,
+            screenSize: {
+                height: 1700,
+                width: 812,
+            },
+            screenshotHeight: 1700,
+            screenshotWidth: 812,
+            isPortrait: true,
+            iosRectangles: {
+                STATUS_BAR: 20,
+                HOME_BAR: {
+                    PORTRAIT: {
+                        height: 0,
+                        width: 0,
+                        x: 0,
+                        y: 0,
+                    },
+                },
+            },
         })
         ignoreRectanglesOptions = {
             blockOuts: [],
@@ -59,7 +78,12 @@ describe('rectangles', () => {
         })
 
         it('should get the block outs', async () => {
-            ignoreRectanglesOptions.blockOuts = [ { x: 11, y: 22, width: 33, height: 44 }, { x: 21, y: 32, width: 43, height: 54 } ]
+            ignoreRectanglesOptions.blockOuts = [{x: 11, y: 22, width: 33, height: 44}, {
+                x: 21,
+                y: 32,
+                width: 43,
+                height: 54
+            }]
 
             expect(await Rectangles.determineIgnoreRectangles(IMAGE_STRING, ignoreRectanglesOptions)).toMatchSnapshot()
         })
@@ -95,8 +119,8 @@ describe('rectangles', () => {
             await Rectangles.determineIgnoreRectangles(IMAGE_STRING, ignoreRectanglesOptions)
 
             expect(global.browser.getSettings).toBeCalled()
-            expect(global.browser.updateSettings).toBeCalledWith({ allowInvisibleElements: true })
-            expect(global.browser.updateSettings).toHaveBeenLastCalledWith({ allowInvisibleElements: false })
+            expect(global.browser.updateSettings).toBeCalledWith({allowInvisibleElements: true})
+            expect(global.browser.updateSettings).toHaveBeenLastCalledWith({allowInvisibleElements: false})
         })
 
         it('should call the settings API for android when the navigation bar needs to be blocked out', async () => {
@@ -106,55 +130,89 @@ describe('rectangles', () => {
             await Rectangles.determineIgnoreRectangles(IMAGE_STRING, ignoreRectanglesOptions)
 
             expect(global.browser.getSettings).toBeCalled()
-            expect(global.browser.updateSettings).toBeCalledWith({ allowInvisibleElements: true })
-            expect(global.browser.updateSettings).toHaveBeenLastCalledWith({ allowInvisibleElements: false })
+            expect(global.browser.updateSettings).toBeCalledWith({allowInvisibleElements: true})
+            expect(global.browser.updateSettings).toHaveBeenLastCalledWith({allowInvisibleElements: false})
         })
 
         it('should call the settings API for android when elements needs to be blocked out', async () => {
             global.browser.isAndroid = true
-            ignoreRectanglesOptions.elementBlockOuts = [ { element: { selector: 'selector-1' } }, {
-                element: { selector: 'selector-1' }
-            } ]
+            ignoreRectanglesOptions.elementBlockOuts = [{element: {selector: 'selector-1'}}, {
+                element: {selector: 'selector-1'}
+            }]
             await Rectangles.determineIgnoreRectangles(IMAGE_STRING, ignoreRectanglesOptions)
             expect(global.browser.getSettings).toBeCalled()
-            expect(global.browser.updateSettings).toBeCalledWith({ allowInvisibleElements: true })
-            expect(global.browser.updateSettings).toHaveBeenLastCalledWith({ allowInvisibleElements: false })
+            expect(global.browser.updateSettings).toBeCalledWith({allowInvisibleElements: true})
+            expect(global.browser.updateSettings).toHaveBeenLastCalledWith({allowInvisibleElements: false})
         })
     })
 
     describe('determineStatusBarRectangles', () => {
-        it('should determine the statusbar rectangles', async () => {
-            global.$ = () => [ { 'element-selector-1': {} } ]
+        it('should determine the initial statusbar rectangles', async () => {
+            expect(await Rectangles.determineStatusBarRectangles({
+                dpr: 4,
+                iosStatusBar: 20,
+                screenshotWidth: 300,
+            })).toMatchSnapshot()
+        })
 
-            expect(await Rectangles.determineStatusBarRectangles(4, 300)).toMatchSnapshot()
-            expect(await Rectangles.determineStatusBarRectangles(1, 200)).toMatchSnapshot()
+        it('should determine the statusbar rectangles by using the already defined one', async () => {
+            expect(await Rectangles.determineStatusBarRectangles({
+                dpr: 10,
+                iosStatusBar: 20,
+                screenshotWidth: 30,
+            })).toMatchSnapshot()
+
+            // Call it again, this should not alter the outcome
+            expect(await Rectangles.determineStatusBarRectangles({
+                dpr: 40,
+                iosStatusBar: 50,
+                screenshotWidth: 60,
+            })).toMatchSnapshot()
+        })
+
+        it('should determine the iOS statusbar rectangles', async () => {
+            global.$ = () => [{'element-selector-1': {}}]
+            global.browser.isIOS = true
+
+            expect(await Rectangles.determineStatusBarRectangles({
+                dpr: 4,
+                iosStatusBar: 20,
+                screenshotWidth: 300,
+            })).toMatchSnapshot()
         })
     })
 
     describe('determineNavigationBarRectangles', () => {
         it('should determine the navigationbar rectangles', async () => {
-            global.$ = () => [ { 'element-selector-1': {} } ]
+            global.$ = () => [{'element-selector-1': {}}]
 
             expect(await Rectangles.determineNavigationBarRectangles(2, 500)).toMatchSnapshot()
-            expect(await Rectangles.determineNavigationBarRectangles(1, 200)).toMatchSnapshot()
+        })
+
+        it('should determine the navigationbar rectangles by using the already defined one', async () => {
+            global.$ = () => [{'element-selector-1': {}}]
+
+            expect(await Rectangles.determineNavigationBarRectangles(2, 500)).toMatchSnapshot()
+            // Call it again, this should not alter the outcome
+            expect(await Rectangles.determineNavigationBarRectangles(4, 1000)).toMatchSnapshot()
         })
     })
 
     describe('determineValidBlockOuts', () => {
         it('should return the valid blockouts if an valid array if provided', () => {
             const blockOuts = [
-                { x: 1, y: 2, width: 3, height: 4 },
-                { x: 1, y: 2, width: 3, height: 4 },
+                {x: 1, y: 2, width: 3, height: 4},
+                {x: 1, y: 2, width: 3, height: 4},
             ]
 
             expect(Rectangles.determineValidBlockOuts(blockOuts)).toMatchSnapshot()
         })
         it('should return the valid blockouts if an array with invalid keys is provided', () => {
             const blockOuts = [
-                { x: 5, y: 6, width: 7, height: 8 },
-                { b: 5, y: 6, widthy: 7, height: 8 },
-                { x: 9, y: 12, width: 13, height: 14 },
-                { x: 9, ho: 12, width: 13, bar: 14 },
+                {x: 5, y: 6, width: 7, height: 8},
+                {b: 5, y: 6, widthy: 7, height: 8},
+                {x: 9, y: 12, width: 13, height: 14},
+                {x: 9, ho: 12, width: 13, bar: 14},
             ]
 
             expect(Rectangles.determineValidBlockOuts(blockOuts)).toMatchSnapshot()
@@ -163,35 +221,35 @@ describe('rectangles', () => {
 
     describe('determineElementBlockOuts', () => {
         it('should return an empty array if no elements are provided', async () => {
-            expect(await Rectangles.determineElementBlockOuts([], { dpr: 1 })).toMatchSnapshot()
+            expect(await Rectangles.determineElementBlockOuts([], {dpr: 1})).toMatchSnapshot()
         })
 
         it('should return an array of one element blockout if 1 element is provided', async () => {
             const selector = 'element-selector-1'
             const components = [
-                { element: { selector } }
+                {element: {selector}}
             ]
-            global.$$ = () => [ { 'element-selector-1': {} } ]
+            global.$$ = () => [{'element-selector-1': {}}]
 
-            expect(await Rectangles.determineElementBlockOuts(components, { dpr: 1 })).toMatchSnapshot()
+            expect(await Rectangles.determineElementBlockOuts(components, {dpr: 1})).toMatchSnapshot()
         })
 
         it('should return an array of one element blockout if multiple elements are provided but 1 with an element number', async () => {
             const selector = 'element-selector-1'
             const components = [
-                { element: { selector }, elementNumber: 1 }
+                {element: {selector}, elementNumber: 1}
             ]
-            global.$$ = () => [ { 'element-selector-1': {} }, { 'element-selector-1': {} }, { 'element-selector-1': {} } ]
+            global.$$ = () => [{'element-selector-1': {}}, {'element-selector-1': {}}, {'element-selector-1': {}}]
 
-            expect(await Rectangles.determineElementBlockOuts(components, { dpr: 1 })).toMatchSnapshot()
+            expect(await Rectangles.determineElementBlockOuts(components, {dpr: 1})).toMatchSnapshot()
         })
 
         it('should return an array of element blockouts if multiple elements are provided', async () => {
             const selector = 'element-selector-1'
-            const components = [ { element: { selector } }, { element: { selector } }, { element: { selector } } ]
-            global.$$ = () => [ { 'element-selector-1': {} }, { 'element-selector-1': {} }, { 'element-selector-1': {} } ]
+            const components = [{element: {selector}}, {element: {selector}}, {element: {selector}}]
+            global.$$ = () => [{'element-selector-1': {}}, {'element-selector-1': {}}, {'element-selector-1': {}}]
 
-            expect(await Rectangles.determineElementBlockOuts(components, { dpr: 1 })).toMatchSnapshot()
+            expect(await Rectangles.determineElementBlockOuts(components, {dpr: 1})).toMatchSnapshot()
         })
     })
 
@@ -227,7 +285,7 @@ describe('rectangles', () => {
     describe('getElementRectangles', () => {
         it('should return element rectangles', async () => {
 
-            expect(await Rectangles.getElementRectangles({ elementId: 1 })).toMatchSnapshot()
+            expect(await Rectangles.getElementRectangles({elementId: 1})).toMatchSnapshot()
             expect(global.browser.getElementRect).toBeCalledWith(1)
         })
     })
