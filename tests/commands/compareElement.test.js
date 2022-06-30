99ac33cf13bd0230c51a506dcececb582ffdf72a
Wim Selles <wim@wswebcreation.nl>
2022-06-30 12:27:39 +0300

    fix: fix element blockout options
    this was broken

--- tests/commands/compareElement.test.js
+++ tests/commands/compareElement.test.js
@@ -1,30 +1,37 @@
 import compareElement from '../../lib/commands/compareElement'
 import * as Options from '../../lib/helpers/options'
 import * as BaseLine from '../../lib/methods/checkBaselineImageExists'
+import * as DetermineValidBlockOuts from '../../lib/methods/rectangles'
 import * as Compare from '../../lib/methods/executeCompare'
 
 describe('compareElement', () => {
-    let checkBaselineImageExistsSpy, executeCompareSpy, instanceCompareOptionsSpy
+    let checkBaselineImageExistsSpy,
+        determineValidBlockOutsSpy,
+        executeCompareSpy,
+        instanceCompareOptionsSpy
     const saveElementData = {
         base64Screenshot: '1234djdnfjkn',
         fileName: 'save-element-filename.png',
         folders: {
             actual: 'usr/save-element-filename-folder',
-        }
+        },
     }
 
     beforeEach(() => {
         global.driver = {
             saveElement: jest.fn().mockResolvedValue(saveElementData),
         }
-        checkBaselineImageExistsSpy = jest.spyOn(BaseLine, 'checkBaselineImageExists').mockResolvedValue()
+        checkBaselineImageExistsSpy = jest
+            .spyOn(BaseLine, 'checkBaselineImageExists')
+            .mockResolvedValue()
     })
 
     afterEach(() => {
         // restore the spies
-        instanceCompareOptionsSpy.mockRestore()
         checkBaselineImageExistsSpy.mockRestore()
+        determineValidBlockOutsSpy.mockRestore()
         executeCompareSpy.mockRestore()
+        instanceCompareOptionsSpy.mockRestore()
         global.driver = {
             saveElement: jest.fn().mockRestore(),
         }
@@ -33,47 +40,103 @@ describe('compareElement', () => {
     it('should be able to return compare element data', async function () {
         const options = { a: 1, b: true, c: 'string' }
 
-        instanceCompareOptionsSpy = jest.spyOn(Options, 'instanceCompareOptions').mockReturnValue({})
-        executeCompareSpy = jest.spyOn(Compare, 'executeCompare').mockResolvedValue({
-            fileName: saveElementData.fileName,
-            folders: saveElementData.folders,
-            misMatchPercentage: 0,
-        })
+        instanceCompareOptionsSpy = jest
+            .spyOn(Options, 'instanceCompareOptions')
+            .mockReturnValue({})
+        executeCompareSpy = jest
+            .spyOn(Compare, 'executeCompare')
+            .mockResolvedValue({
+                fileName: saveElementData.fileName,
+                folders: saveElementData.folders,
+                misMatchPercentage: 0,
+            })
+        determineValidBlockOutsSpy = jest.spyOn(
+            DetermineValidBlockOuts,
+            'determineValidBlockOuts'
+        )
 
-        const data = await compareElement.call({
-            compareOptions: options,
-            folders: { actual: saveElementData.folders.actual },
-            autoSaveBaseline: false
-        }, {}, 'tag')
+        const data = await compareElement.call(
+            {
+                compareOptions: options,
+                folders: { actual: saveElementData.folders.actual },
+                autoSaveBaseline: false,
+            },
+            {},
+            'tag'
+        )
 
         expect(instanceCompareOptionsSpy).toHaveBeenCalled()
         expect(global.driver.saveElement).toBeCalledWith({}, 'tag', {})
         expect(checkBaselineImageExistsSpy).toHaveBeenCalled()
-        expect(executeCompareSpy).toBeCalledWith(saveElementData.folders, saveElementData.fileName, options)
+        expect(determineValidBlockOutsSpy).not.toHaveBeenCalled()
+        expect(executeCompareSpy).toBeCalledWith(
+            saveElementData.folders,
+            saveElementData.fileName,
+            options
+        )
         expect(data).toMatchSnapshot()
     })
 
     it('should be able to return compare screen data with instance options are provided', async function () {
-        const options = { a: 1, b: true, c: 'string' }
-        const instanceOptions = { a: 2, b: false, c: 'bar' }
+        const blockOutsValue = [
+            {
+                height: 1,
+                width: 2,
+                x: 3,
+                y: 4,
+            },
+        ]
+        const options = {
+            a: 1,
+            b: true,
+            c: 'string',
+            blockOuts: blockOutsValue,
+        }
+        const instanceOptions = {
+            a: 2,
+            b: false,
+            c: 'bar',
+            blockOuts: blockOutsValue,
+            output: {},
+        }
 
-        instanceCompareOptionsSpy = jest.spyOn(Options, 'instanceCompareOptions').mockReturnValue(instanceOptions)
-        executeCompareSpy = jest.spyOn(Compare, 'executeCompare').mockResolvedValue({
-            fileName: saveElementData.fileName,
-            folders: saveElementData.folders,
-            misMatchPercentage: 0,
-        })
+        instanceCompareOptionsSpy = jest
+            .spyOn(Options, 'instanceCompareOptions')
+            .mockReturnValue(instanceOptions)
+        executeCompareSpy = jest
+            .spyOn(Compare, 'executeCompare')
+            .mockResolvedValue({
+                fileName: saveElementData.fileName,
+                folders: saveElementData.folders,
+                misMatchPercentage: 0,
+            })
+        determineValidBlockOutsSpy = jest
+            .spyOn(DetermineValidBlockOuts, 'determineValidBlockOuts')
+            .mockReturnValue(blockOutsValue)
 
-        const data = await compareElement.call({
-            compareOptions: options,
-            folders: { actual: saveElementData.folders.actual },
-            autoSaveBaseline: false
-        }, {}, 'tag', {})
+        const data = await compareElement.call(
+            {
+                compareOptions: options,
+                folders: { actual: saveElementData.folders.actual },
+                autoSaveBaseline: false,
+            },
+            {},
+            'tag',
+            {}
+        )
 
         expect(instanceCompareOptionsSpy).toHaveBeenCalled()
         expect(global.driver.saveElement).toBeCalledWith({}, 'tag', {})
         expect(checkBaselineImageExistsSpy).toHaveBeenCalled()
-        expect(executeCompareSpy).toBeCalledWith(saveElementData.folders, saveElementData.fileName, instanceOptions)
+        expect(determineValidBlockOutsSpy).toBeCalledWith(blockOutsValue)
+        expect(executeCompareSpy).toBeCalledWith(
+            saveElementData.folders,
+            saveElementData.fileName,
+            {
+                ...instanceOptions,
+                ...{ output: { ignoredBoxes: blockOutsValue } },
+            }
+        )
         expect(data).toMatchSnapshot()
     })
 })
