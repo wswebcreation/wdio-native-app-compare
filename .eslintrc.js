5e13cea8901dd99bc81a8c9250dddd1c90e808fb
Wim Selles <wim@wswebcreation.nl>
2022-07-01 14:07:33 +0200

    chore: update all babel packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,5 @@
 module.exports = {
-    extends: [
-        'eslint:recommended',
-        'plugin:import/errors',
-        'plugin:import/warnings',
-    ],
+    extends: ['eslint:recommended'],
     env: {
         node: true,
         es6: true,
@@ -23,11 +19,5 @@ module.exports = {
         semi: ['error', 'never'],
         quotes: ['error', 'single'],
         indent: [2, 4],
-
-        'import/no-unresolved': [2, { commonjs: true, amd: true }],
-        'import/named': 2,
-        'import/namespace': 2,
-        'import/default': 2,
-        'import/export': 2,
     },
 }
