bcb5edd18cdd1a9e6d22cc57cdf812180b152ead
Arjan Blok <arjan.blok@gmail.com>
2022-10-28 00:00:27 +0200

    Add typescript typings

---
+++ types/index.d.ts
@@ -0,0 +1,127 @@
+export {}
+
+declare global {
+    namespace WebdriverIO {
+        interface ServiceOption extends PluginOptions {}
+
+        interface Browser {
+            /**
+             * Saves an image of an element
+             */
+            saveElement(
+                element: WebdriverIO.Element,
+                tag: string,
+                options?: SaveElementMethodOptions
+            ): Promise<SaveMethodOutput>
+
+            /**
+             * Saves an image of a screen
+             */
+            saveScreen(tag: string): Promise<SaveMethodOutput>
+
+            /**
+             * Compare an image of an element
+             */
+            compareElement(
+                element: WebdriverIO.Element,
+                tag: string,
+                options?: CompareElementMethodOptions
+            ): Promise<CompareMethodOutput>
+
+            /**
+             * Compares an image of a screen
+             */
+            compareScreen(
+                tag: string,
+                options?: CompareScreenMethodOptions
+            ): Promise<CompareMethodOutput>
+        }
+    }
+}
+
+interface PluginOptions extends CompareOptions, CompareScreenOptions {
+    baselineFolder: string
+    screenshotPath: string
+    autoSaveBaseline?: boolean
+    debug?: boolean
+    imageNameFormat?: string
+    savePerDevice?: boolean
+    output?: {
+        largeImageThreshold?: number
+        errorColor?: {
+            red: number
+            green: number
+            blue: number
+        }
+    }
+}
+
+interface SaveElementMethodOptions {
+    resizeDimensions?: ResizeDimensions
+}
+
+interface CompareElementMethodOptions extends CompareOptions {
+    resizeDimensions?: ResizeDimensions
+}
+
+interface CompareScreenMethodOptions
+    extends CompareOptions,
+        CompareScreenOptions {
+    elementBlockOuts?: ElementBlockOuts[]
+}
+
+interface CompareOptions {
+    blockOuts?: Blockouts[]
+    ignoreAlpha?: boolean
+    ignoreAntialiasing?: boolean
+    ignoreColors?: boolean
+    ignoreLess?: boolean
+    ignoreNothing?: boolean
+    ignoreTransparentPixel?: boolean
+    largeImageThreshold?: number
+    rawMisMatchPercentage?: boolean
+    saveAboveTolerance?: number
+}
+
+interface CompareScreenOptions {
+    blockOutStatusBar?: boolean
+    blockOutIphoneHomeBar?: boolean
+    blockOutNavigationBar?: boolean
+}
+
+interface ResizeDimensions {
+    top?: number
+    right?: number
+    bottom?: number
+    left?: number
+}
+
+interface Blockouts {
+    height: number
+    width: number
+    x: number
+    y: number
+}
+
+interface ElementBlockOuts {
+    element: WebdriverIO.Element | WebdriverIO.Element[]
+    margin?: number
+    elementNumber?: number
+}
+
+interface SaveMethodOutput {
+    fileName: string
+    folders: {
+        actual: string
+    }
+}
+
+interface CompareMethodOutput {
+    fileName: string
+    folders: {
+        actual: string
+        baseline: string
+        diff: string
+    }
+    misMatchPercentage: number
+}
